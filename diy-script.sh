a93e9088e54641b01f49e00e16a659771355b930
tinkyo <tinkyo@qq.com>
2025-01-31 09:57:25 +0800

    自定义

--- diy-script.sh
+++ diy-script.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # 修改默认IP
-sed -i 's/192.168.123.251/10.0.0.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.123.251/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
-sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
 #rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/packages/net/msd_lite
-#rm -rf feeds/packages/net/smartdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
 #rm -rf feeds/luci/themes/luci-theme-argon
-#rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/themes/luci-theme-netgear
 #rm -rf feeds/luci/applications/luci-app-mosdns
-#rm -rf feeds/luci/applications/luci-app-netdata
-#rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
